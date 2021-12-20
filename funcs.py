eea3d24174a7ce9aafa28008bad9e8f05ffda753
ei-se <qianhui.jin@yahoo.fr>
2021-12-20 12:46:08 +0100

    ajouter la fonction et le test pour calculer le médiane et corriger quelque partie du code pour les autres fonctions

--- funcs.py
+++ funcs.py
@@ -1,3 +1,5 @@
+import math
+
 def min_int(a,b):
     if a < b:
    	    return a
@@ -6,8 +8,18 @@ def min_int(a,b):
 
 def moy_int(l=[]):
     s = sum(l)
-    n = len(l)
+    n = float(len(l))
     moy = s/n
     return moy
 
+def med_int(l=[]):
+    n = len(l)
+    m = n//2
+    l.sort()
+    if n < 1:
+        return None
+    elif n % 2 == 1 :
+        return l[m]
+    else:
+        return ( l[m] + l[m-1] ) / 2.0
 
--- tests_funcs.py
+++ tests_funcs.py
@@ -12,15 +12,16 @@ class TestFuncs(unittest.TestCase):
     def test_moy_int(self):
         self.assertEqual(funcs.moy_int([0,1,2,3]),1.5)
         self.assertEqual(funcs.moy_int([-1,-5,-12]),-6)
-        self.assertEqual(funcs.moy_int([-5,1,-2,3]),-1)
+        self.assertEqual(funcs.moy_int([-5,1,-2,3]),-0.75)
         self.assertEqual(funcs.moy_int([0,0,0,0]),0)
 
-    #def test_med_int(self):
-       # self.assertEqual(funcs.med_int(2,2,2), 2)
-       # self.assertEqual(funcs.med_int(1,2,3), 2)
-       # self.assertEqual(funcs.med_int(6,4,5), 5)
-       # self.assertEqual(funcs.med_int(8,-3,11), 8)
+    def test_med_int(self):
+        self.assertEqual(funcs.med_int([2,2,2,2]),2)
+        self.assertEqual(funcs.med_int([1,2,3]),2)
+        self.assertEqual(funcs.med_int([6,4,5,7]),5.5)
+        self.assertEqual(funcs.med_int([8,-3,11]),8)
+        self.assertEqual(funcs.med_int([1,5,3,9,10,7]),6)
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
         unittest.main()
 
