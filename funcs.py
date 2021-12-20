b95319333a23cd7e5901dc71868d2b80b24a7a8c
ei-se <qianhui.jin@yahoo.fr>
2021-12-20 12:15:47 +0100

    ajouter la fonction et le test pour calculer le minimum

--- funcs.py
+++ funcs.py
@@ -1,4 +1,8 @@
-import math
+def min_int(a,b):
+    if a < b:
+   	    return a
+    else:
+        return b
 
 def moy_int(l=[]):
     s = sum(l)
--- tests_funcs.py
+++ tests_funcs.py
@@ -1,18 +1,26 @@
 import funcs
 import unittest
 
-def test_moy_int(self):
-    self.assertEqual(funcs.moy_int([0,1,2,3]),1.5)
-    self.assertEqual(funcs.moy_int([-1,-5,-12]),-6)
-    self.assertEqual(funcs.moy_int([-5,1,-2,3]),-1)
-    self.assertEqual(funcs.moy_int([0,0,0,0]),0)
+class TestFuncs(unittest.TestCase):
 
-def test_med_int(self):
-   self.assertEqual(funcs.med_int(2,2,2), 2)
-   self.assertEqual(funcs.med_int(1,2,3), 2)
-   self.assertEqual(funcs.med_int(6,4,5), 5)
-   self.assertEqual(funcs.med_int(8,-3,11), 8)
+    def test_min_int(self):
+        self.assertEqual(funcs.min_int(0,2),0)
+        self.assertEqual(funcs.min_int(-1,-5),-5)
+        self.assertEqual(funcs.min_int(-1,2),-1)
+        self.assertEqual(funcs.min_int(0,0),0)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_moy_int(self):
+        self.assertEqual(funcs.moy_int([0,1,2,3]),1.5)
+        self.assertEqual(funcs.moy_int([-1,-5,-12]),-6)
+        self.assertEqual(funcs.moy_int([-5,1,-2,3]),-1)
+        self.assertEqual(funcs.moy_int([0,0,0,0]),0)
+
+    #def test_med_int(self):
+       # self.assertEqual(funcs.med_int(2,2,2), 2)
+       # self.assertEqual(funcs.med_int(1,2,3), 2)
+       # self.assertEqual(funcs.med_int(6,4,5), 5)
+       # self.assertEqual(funcs.med_int(8,-3,11), 8)
+
+    if __name__ == '__main__':
+        unittest.main()
 
