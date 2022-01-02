8ba63c0a5e38c286baa35ed2db313661d4f187fe
ei-se <qianhui.jin@yahoo.fr>
2022-01-02 23:13:23 +0100

    ajout d'une fonction pour calculer la médiane à partir d'une liste entier et son test

--- funcs.py
+++ funcs.py
@@ -11,3 +11,14 @@ def moy_int(l=[]):
     n = float(len(l))
     moy = s/n
     return moy
+
+def med_int(l=[]):
+    n = len(l)
+    m = n//2
+    l.sort()
+    if n < 1:
+        return None
+    elif n % 2 == 1 :
+        return l[m]
+    else:
+        return ( l[m] + l[m-1] ) / 2.0
--- tests_funcs.py
+++ tests_funcs.py
@@ -15,6 +15,13 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.moy_int([-5,1,-2,3]),-0.75)
         self.assertEqual(funcs.moy_int([0,0,0,0]),0)
 
+    def test_med_int(self):
+        self.assertEqual(funcs.med_int([2,2,2,2]),2)
+        self.assertEqual(funcs.med_int([1,2,3]),2)
+        self.assertEqual(funcs.med_int([6,4,5,7]),5.5)
+        self.assertEqual(funcs.med_int([8,-3,11]),8)
+        self.assertEqual(funcs.med_int([1,5,3,9,10,7]),6)
+
 if __name__ == '__main__':
         unittest.main()
 
