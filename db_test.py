94b47b92f071140c37934149196a6470102048ca
lynne <jan.kry959@gmail.com>
2022-01-03 16:54:47 +0100

    1er version

--- db_test.py
+++ db_test.py
@@ -4,11 +4,8 @@ import unittest
 class TestFuncs(unittest.TestCase):
 
     def test_min_int(self):
-        
         self.assertEqual(bibli.checkdata(), True)
         self.assertEqual(bibli.check_all_data(), None)
-        #self.assertEqual(bibli.min_int(-1,2),-1)
-        #self.assertEqual(bibli.min_int(0,0),0)
 
 if __name__ == '__main__':
         unittest.main()
